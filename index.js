9e16a482d05d20d9d0e48869238278bacc1e25d9
LuXXraYy <63782513+LuXXraYy@users.noreply.github.com>
2021-05-24 19:04:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function Display(){
+    var start = document.getElementById("disImage");
+    var image = document.createElement("IMG");
+    var InputNum = parseInt(document.getElementById("input").value);
+    var child = start.childNodes;
+    var length = child.length;
+    var id = (length + 1).toString(10);
+    var Source = id + "-Number-PNG.png";
+    image.src = Source;
+    image.height = 100;
+    image.width = 100;
+    image.style.border = "5px solid white";
+    image.style.padding = "10px";
+
+    if(length < InputNum){
+        start.appendChild(image);
+        length++;
+    }
+    else if(length > InputNum){
+        start.removeChild(child[length-1]);
+        length--;
+    }
+    if(length !== InputNum){
+        setTimeout(Display,1000);
+    }
+}
